2ee57230eca6ee40695d4b60ed7d4dfd49d26b26
mcrae.su <mcrae.su@trustlife.com>
2023-04-06 14:52:13 +0800

    拓展 spring-boot-gradle-plugin 插件

--- src/main/java/com/atguigu/boot/user/controller/UserController.java
+++ src/main/java/com/atguigu/boot/user/controller/UserController.java
@@ -1,2 +1,16 @@
-package com.atguigu.boot.user.controller;public class UserController {
+package com.atguigu.boot.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/actor")
+public class UserController {
+
+    @GetMapping("/name")
+    public String getActorName(){
+
+        return "梁朝伟";
+    }
 }
